8f35d6a4588055e252cfdd8341f99faa06451877
WeynAmarock <64893257+WeynAmarock@users.noreply.github.com>
2020-05-09 17:24:57 +0200

    Update generateChamps.php

--- php/generateChamps.php
+++ php/generateChamps.php
@@ -1,44 +1,64 @@
 <?php
-
 function generateChamps($type, $id){
 
-    $divG='<legend>Paramétrage du champ n\''. $id .' de type '.$type. ':</legend>
-    <div name="num_champ">
-        <!-- Récupérer le numéro de champ en php pour l\'afficher -->
-    </div>
-    <div name="nom_int" class="elem_form">
+    $textLegend=
+    '<legend>Paramétrage du champ n\''.$id.' de type '.$type.':</legend>';
+
+    $textNom=
+    '<div name="nom" class="elem_form">
         <label for="'.$type.'_name'.$id.'">Nom du champ </label>
         <input type="text" name="'.$type.'_name'.$id.'" class="champ">
-    </div>
+    </div>';
 
-    <div id="size" class="elem_form">
+    $textSize=
+    '<div id="size" class="elem_form">
         <label for="'.$type.'_size'.$id.'">Taille du champ </label>
         <input type="int" name="'.$type.'_size'.$id.'" class="champ">
-    </div>
+    </div>';
 
-    <div id="values" class="elem_form">
+    $textValExtreme=
+    '<div id="values" class="elem_form">
         <label>Valeurs extrêmes</label>
-        <input type="int" id="'.$type.'_min'.$id.'" class="champ_b">
-        <input type="int" id="'.$type.'_max'.$id.'" class="champ_b">
-    </div>
+        <input type='.$type.' name="'.$type.'_min'.$id.'" class="champ_b">
+        <input type='.$type.' name="'.$type.'_max'.$id.'" class="champ_b">
+    </div>';
 
-</fieldset>
-</br>';
+    $textList=
+    '<div id="liste_valeurs">
+        <label for="liste'.$type.' '.$id.'">Liste de valeurs</label>
+        <input type="file" name="liste'.$type.'_liste'.$id.'" class="champ">
+    </div>';
 
-    /*switch($type){
-        
+    $textFile=
+    '<div id="fichier">
+        <label for="fichier'.$type.$id.'">Fichier</label>
+        <input type="file" name="fichier'.$type.'_file'.$id.'" class="champ">
+    </div>';
+
+    switch($type){
         case "int":
-
-            echo 
+        case "double_float":
+        case "tinyint":
+            echo $textLegend.$textNom.$textSize.$textValExtreme.$textList.'</br>';
         break;
 
-        case 
-    } */
+        case "datetime":
+        case "time":
+        case "date":
+            echo $textLegend.$textNom.$textValExtreme.$textList.'</br>';
 
-    echo $divG;
+        case "char":
+        case "varchar":
+
+            echo $textLegend.$textNom.$textSize.$textFile.'</br>';
+        break;
+        
+        case "boolean":
+            echo $textLegend.$textNom.'</div>';
+        break;
+
+    }
 }
-
-
 
 
 
