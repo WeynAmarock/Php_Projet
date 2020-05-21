84984f7f21b510ca0027cbde7f4bdd33dd8d2155
WeynAmarock <64893257+WeynAmarock@users.noreply.github.com>
2020-05-21 11:10:29 +0200

    Update Modele.php

--- php/Modele.php
+++ php/Modele.php
@@ -1,65 +1,54 @@
-<?php
+<<?php
 
 class Modele {
-    private $nom_Modele;
-    private $libele;
-    private $dateCreation;
-    private $typeFile;
+    private $libelle;
+    private $nom_table;
+    private $nom_fichier;
+    private $date_creation;
+    private $type_File;
 
-    /*public function __construct($nom_M,$type_M,$nb_ligne,$tab_champs){
-        $this->nom_Modele=$nom_M;
-        $this->type_File=$type_M;
-        $this->nbLigne=$nb_Ligne;
-        $this->tab_champs=$tab_champs;
-        $this->date_Creation = date('d-m-y h:i:s');
-
-    }*/
-
-    public function __construct(){
-        
+   
+    public function constructModele($nom,$type,$id_Libele){
+        $this->nom_table = $nom;
+        $this->libele=$id_Libele.'_'.$nom;
+        $this->nom_fichier=$nom.$type;
+        $this->type_file = $type;
+        $this->date_creation = date('d-m-y h:i:s');
     }
     
 
     public function getNom(){
-        return $this->nom_Modele;
+        return $this->nom_table;
     }
     public function getType(){
         return $this->type_File;
     }
-    public function getCdate(){
-        return $this->dateCreation;
+    public function getNomFile(){
+        return $this->nom_fichier;
+    }
+    public function getDate(){
+        return $this->date_creation;
     }
     public function getLibele(){
-        return $this->libele;
+        return $this->libelle;
     }
 
     public function setNom($nom){
-        $this->nom_Modele = $nom;
+        $this->nom_table = $nom;
     }
     public function setType($type){
         $this->type_File = $type;
     }
 
     public function setDate($date){
-        $this->dateCreation = $date;
+        $this->date_creation = $date;
     }
     public function setlibele($lib){
-        $this->libele=$lib;
+        $this->libelle=$lib;
     }
 
-    public function CreateModele($nom,$type){
-        $this->nom_Modele = $nom;
-        $this->type_File = $type;
-        $this->dateCreation = date('d-m-y h:i:s');
-    }
-
-    public function showModele(){
-        echo "Nom modèle : ".$this->nom_Modele;
-        echo "</br>";
-        echo "Date : ";
-        echo $this->dateCreation;
-        echo "</br>";
-        echo "type : " . $this->type_File;
+    public function setNomFile($nom_F){
+        $this->nom_fichier=$nom_F;
     }
     
 
